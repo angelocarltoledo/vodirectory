18bff74607b896cd65223be63a9bc16492698884
angelocarltoledo <angelocarltoledo@gmail.com>
2023-11-25 21:26:36 -0800

    Export fetchData test

--- utils/fetchData.js
+++ utils/fetchData.js
@@ -1,9 +1,9 @@
-const fetchData = async(category) => {
+"use server";
+
+export const fetchData = async(category) => {
     const response = await fetch(process.env[category], { cache: 'no-store' });
     const data = await response.json();
     const cat = Object.keys(data)[0];
 
     return [data, cat];
 }
-
-export default fetchData;
