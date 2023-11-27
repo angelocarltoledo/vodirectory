5ac64c04205bd2aa0425ebe09f413abc1203c5c1
angelocarltoledo <angelocarltoledo@gmail.com>
2023-11-26 19:35:46 -0800

    CoachCards fix 4

--- components/CoachCards.jsx
+++ components/CoachCards.jsx
@@ -24,63 +24,63 @@ const CoachCards = ({data, category}) => {
 
   return (
     <>
-        <input
-            type="text" 
-            onChange={(e) => setSearch(e.target.value.toLowerCase())}
-            placeholder='Search'
-            className='flex-grow rounded-2xl p-2'
-        />
+      <input
+          type="text" 
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
+          placeholder='Search'
+          className='flex-grow rounded-2xl p-2'
+      />
 
-        <div className="flex flex-col gap-5 justify-center">
-          <div className='flex flex-wrap gap-5 justify-center'>
-            <div className='flex gap-2'>
-              <span>Specialty:</span>
-              <select className='p-1' name="specialty" id="specialty" onChange={event => setSpecialty(event.target.value.toLowerCase())}>
-                  <option value="">All</option>
-                  {filterUniqueSpecialties(data).sort().map(entry => (
-                      <option key={entry} value={entry}>{entry}</option>
-                  ))
-                  }
-              </select>
-            </div>
-            <div className='flex gap-2'>
-              <span>Price:</span>
-              <select className='p-1' name="specialty" id="specialty" onChange={event => setOrder(event.target.value)}>
-                  <option value="asc">Ascending</option>
-                  <option value="desc">Descending</option>
-              </select>
-            </div>
+      <div className="flex flex-col gap-5 justify-center">
+        <div className='flex flex-wrap gap-5 justify-center'>
+          <div className='flex gap-2'>
+            <span>Specialty:</span>
+            <select className='p-1' name="specialty" id="specialty" onChange={event => setSpecialty(event.target.value.toLowerCase())}>
+                <option value="">All</option>
+                {filterUniqueSpecialties(data).sort().map(entry => (
+                    <option key={entry} value={entry}>{entry}</option>
+                ))
+                }
+            </select>
           </div>
-
-          <p>NOTE: All featured prices are for private one hour sessions. See individual notes for other services and prices.</p>
+          <div className='flex gap-2'>
+            <span>Price:</span>
+            <select className='p-1' name="specialty" id="specialty" onChange={event => setOrder(event.target.value)}>
+                <option value="asc">Ascending</option>
+                <option value="desc">Descending</option>
+            </select>
+          </div>
         </div>
 
-        <div className="flex flex-wrap gap-5 justify-center">
-            {orderedCoaches.map(entry => (
-            <div key={entry.name} className="flex-grow w-fit p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{entry.name}</h5>
-                {entry.twitter && 
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Twitter/X: {entry.twitter}</p>
-                }
-                {entry.specialty && 
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Specialty: {entry.specialty}</p>
-                }
-                {entry.price !== "0" ?
-                (
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Price: ${entry.price}</p>
-                ) : (
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Price: Not listed</p>
-                )
-                }
-                {entry.notes && 
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Notes: {entry.notes}</p>
-                }
-                <Link href={entry.link} target='_blank' className="inline-flex entrys-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Go to Site!
-                </Link>
-            </div>
+        <p>NOTE: All featured prices are for private one hour sessions. See individual notes for other services and prices.</p>
+      </div>
+
+      <div className="flex flex-wrap w-full gap-5 justify-center">
+        {orderedCoaches.map(entry => (
+        <div key={entry.name} className="flex-grow p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{entry.name}</h5>
+            {entry.twitter && 
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Twitter/X: {entry.twitter}</p>
+            }
+            {entry.specialty && 
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Specialty: {entry.specialty}</p>
+            }
+            {entry.price !== "0" ?
+            (
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Price: ${entry.price}</p>
+            ) : (
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Price: Not listed</p>
+            )
+            }
+            {entry.notes && 
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Notes: {entry.notes}</p>
+            }
+            <Link href={entry.link} target='_blank' className="inline-flex entrys-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Go to Site!
+            </Link>
+        </div>
         ))}
-        </div>
+      </div>
     </>
   )
 }
