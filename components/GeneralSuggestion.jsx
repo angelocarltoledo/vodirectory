7e971d16259bcff6d23d0c97cf46c4fffc0b20fa
angelocarltoledo <angelocarltoledo@gmail.com>
2024-01-10 00:46:19 -0800

    Minor Edit

--- components/GeneralSuggestion.jsx
+++ components/GeneralSuggestion.jsx
@@ -38,7 +38,7 @@ const GeneralSuggestion = ({ category }) => {
               type="text"
               id="subject"
               className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-              placeholder="Director Name"
+              placeholder={category + " Name"}
               required
             />
           </div>
@@ -53,7 +53,7 @@ const GeneralSuggestion = ({ category }) => {
               type="text"
               id="subject"
               className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-              placeholder="@DirectorTwitterUsername"
+              placeholder={"@" + category + "TwitterUsername"}
               required
             />
           </div>
