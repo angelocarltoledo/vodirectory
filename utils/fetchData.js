37ad06c08a7f42393f09a19dd7b2379b554ab11e
angelocarltoledo <angelocarltoledo@gmail.com>
2024-01-10 00:27:16 -0800

    Re-added cache no-store to fetchData

--- utils/fetchData.js
+++ utils/fetchData.js
@@ -1,5 +1,5 @@
 const fetchData = async (category) => {
-  const response = await fetch(process.env[category]);
+  const response = await fetch(process.env[category], { cache: "no-store" });
   const data = await response.json();
   const cat = Object.keys(data)[0];
 
